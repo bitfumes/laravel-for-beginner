daeac0101563aa0a3baf610cfb5eac35b2ae7e51
sarthaksavvy <sarthak@bitfumes.com>
2019-06-22 21:18:21 +0530

    facade explained

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,60 @@
 <?php
+//  What is Facade
+//  Why we need Facade
+//  How to implement our own Facade
 
+class Fish{
+    public function swim()
+    {
+        return 'swimming';
+    }
+    public function eat()
+    {
+        return 'eating';
+    }
+}
+app()->bind('fish',function(){
+    return new Fish;
+});
 
+class Bike{
+    public function start()
+    {
+        return 'starting';
+    }
+}
 
+app()->bind('bike',function(){
+    return new Bike;
+});
 
+class Facade {
+    public static function __callStatic($name,$args)
+    {
+        return app()->make(static::getFacadeAccessor())->$name();
+    }
 
+    protected static function getFacadeAccessor()
+    {
+        // # code...
+    }
+}
 
+class FishFacade extends Facade {
+    protected static function getFacadeAccessor()
+    {
+        return 'fish';
+    }   
+}
 
+class BikeFacade extends Facade {
+    protected static function getFacadeAccessor()
+    {
+        return 'bike';
+    }
+}
 
-
-
-
+dd(BikeFacade::start());
 
 
 
